41a04dd0cd4a704c647e59ce4e92628700d928ff
Rinku2002 <gangishettyrinku@gmail.com>
2023-09-27 01:11:44 +0530

    Initial

---
+++ precache-manifest.9111e6ff960cba798119e15eecf33bf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "011c75cdb6ad5d0593a6",
+    "url": "/static/css/main.2c528796.chunk.css"
+  },
+  {
+    "revision": "011c75cdb6ad5d0593a6",
+    "url": "/static/js/main.011c75cd.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ca7030e56e83d9033ceb1bd4785bdd9",
+    "url": "/index.html"
+  }
+];
